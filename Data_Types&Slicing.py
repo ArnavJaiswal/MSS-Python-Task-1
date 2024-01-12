46f7122a610fd040de53e98cea4a8cf0c2cce6e8
ArnavJaiswal <34517519+ArnavJaiswal@users.noreply.github.com>
2024-01-13 01:57:14 +0530

    Create Data_Types&Slicing.py

---
+++ Data_Types&Slicing.py
@@ -0,0 +1,5 @@
+mixed_data = [10, "apple", 3.14, "orange", 25, 7.5, "banana", 42.0]
+
+print(mixed_data[:3]) #print first 3 elements.
+print(mixed_data[2:5]) #print Elements starting from the 2nd to the 5th position.
+print(mixed_data[-2:]) #print - The last two elements.
